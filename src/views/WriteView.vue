e534f85f1e45c4e15e69de3fd9096554501325ef
cheoljin408 <kcj409@naver.com>
2022-07-31 22:00:45 +0900

    글 작성 테스트

---
+++ src/views/WriteView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+
+</script>
+
+<template>
+  <div>
+    <el-input type="text" placeholder="제목을 입력하세요" />
+  </div>
+  <div class="mt-2">
+    <el-input type="textarea" rows="15" />
+  </div>
+  <div class="mt-2">
+    <el-button type="primary">글 작성 완료</el-button>
+  </div>
+
+</template>
+
+<style>
+
+</style>
